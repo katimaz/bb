2d2d5f5591e35de3b09e437a7db96bfc5549a4cf
Bounbang <51937274+Bounbang@users.noreply.github.com>
2019-08-22 01:01:28 +0800

    Conflict resolved before merge to master

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,10 +3,15 @@
 <div class="btbg"></div>
 <div class="userbg"></div>
 <div class="search-frame">
-    <input name="" type="search">
+    @if(isset($keyword))
+    <input id="input-search" type="search" value="{{$keyword}}">
+    @else
+    <input id="input-search" type="search" value="">
+    @endif
     <div class="search-bt">
         <img src="{{asset("/images/bsearch-bt.png")}}" class="d-none d-sm-block">
-        <img src="{{asset("/images/search-bt.png")}}" class="show-xs"></div>
+        <img src="{{asset("/images/search-bt.png")}}" class="show-xs">
+    </div>
 </div>
 <div class="s-close">
     <img src="{{asset("/images/close.png")}}" width="19" height="20">
@@ -17,39 +22,37 @@
     <!--  登入後隱藏 START-->
     @if(!Auth::check())
     <div class="login-arr">
-        <a href="/login">登入 / 註冊</a>
+        <a href="<?=URL::to('/')?>/login">登入 / 註冊</a>
     </div>
     @else
-    	<div class="button_container">
-            <span class="top"></span>
-            <span class="middle"></span>
-            <span class="bottom"></span>
-        </div>
-        @if(Session::has('offer') && Session::get('offer'))
-        <div class="main-menu">
-            <ul class="menu-ul">
-             <li>
-                <a href="job-reservation.html">我的工作<i class="nl-badge">2</i></a>
-              </li>
-              <li>
-                <a  href="order-statu.html">我的訂單<i class="nl-badge">1</i></a>
-              </li>
-              <li>
-                <a  href="h-set.html">服務設定</a>
-              </li>
-               <li class="submenu" id="subA">
-               <a href="javascript:void(0)">
-                 我的帳務<i class="fa fa-angle-down" aria-hidden="true"></i>
+    <!--登入後顥示選單-->
+    <div class="button_container">
+        <span class="top"></span>
+        <span class="middle"></span>
+        <span class="bottom"></span>
+    </div>
+    <div class="main-menu">
+        @if(session()->get('usr_type') == 0)
+        <ul class="menu-ul">
+            <li>
+                <a href="{{URL::to('/')}}/web/management">我的需求</a>
+            </li>
+            <li>
+                <a href="order.html">訂單狀態</a>
+            </li>
+            <li class="submenu" id="subA">
+                <a href="javascript:void(0)">
+                    我的帳務<i class="fa fa-angle-down" aria-hidden="true"></i>
                 </a>
-                  <div class="sub-list" id="boxA">
-                  <a class="sub-item" href="feedback.html">回饋金</a>
-                  <a class="sub-item" href="income.html">服務收入</a>
-                  <a class="sub-item" href="pay.html">支出</a>
+                <div class="sub-list" id="boxA">
+                    <a class="sub-item" href="feedback.html">回饋金</a>
+                    <a class="sub-item" href="income.html">服務收入</a>
+                    <a class="sub-item" href="pay.html">支出</a>
                 </div>
-              </li>
-              <li class="submenu" id="subB">
-                <a  href="javascript:void(0)">
-                 推薦賺回饋<i class="fa fa-angle-down" aria-hidden="true"></i>
+            </li>
+            <li class="submenu" id="subB">
+                <a href="javascript:void(0)">
+                    推薦賺回饋<i class="fa fa-angle-down" aria-hidden="true"></i>
                 </a>
                 <div class="sub-list" id="boxB">
                   <a class="sub-item" href="javascript:void(0)" @click="set_recommend">推薦連結</a>
@@ -57,68 +60,68 @@
                   <a class="sub-item" href="{{URL::to('/web/partners')}}">我的夥伴團隊</a>
                   <a class="sub-item" href="total.html">我的夥伴團隊總營收</a>
                 </div>
-              </li>  
-              <li>
+            </li>
+            <li>
+                <a href="best-choice.html">我的首選</a>
+            </li>
+            <li>
                 <a  href="calendar.html">我的行事曆</a>
-              </li>
-            </ul>  
-  		</div>
+            </li>
+        </ul>
         @else
-        <!--登入後顥示選單-->
-        <div class="main-menu">
-            <ul class="menu-ul">
-                <li>
-                    <a href="reservation.html">我的需求</a>
-                </li>
-                <li>
-                    <a href="order.html">訂單狀態</a>
-                </li>
-                <li class="submenu" id="subA">
-                    <a href="javascript:void(0)">
-                        我的帳務<i class="fa fa-angle-down" aria-hidden="true"></i>
-                    </a>
-                    <div class="sub-list" id="boxA">
-                        <a class="sub-item" href="feedback.html">回饋金</a>
-                        <a class="sub-item" href="income.html">服務收入</a>
-                        <a class="sub-item" href="pay.html">支出</a>
-                    </div>
-                </li>
-                <li class="submenu" id="subB">
-                    <a href="javascript:void(0)">
-                        推薦賺回饋<i class="fa fa-angle-down" aria-hidden="true"></i>
-                    </a>
-                    <div class="sub-list" id="boxB">
-                        <a class="sub-item" href="javascript:void(0)" @click="set_recommend">推薦連結</a>
-                        <a class="sub-item" href="promote.html">介紹幫棒給新夥伴</a>
-                        <a class="sub-item" href="{{URL::to('/web/partners')}}">我的夥伴團隊</a>
-                        <a class="sub-item" href="total.html">我的夥伴團隊總營收</a>
-                    </div>
-                </li>
-                <li>
-                    <a href="best-choice.html">我的首選</a>
-                </li>
-                <li>
+        <ul class="menu-ul">
+            <li>
+                <a href="{{URL::to('/')}}/web/job_manager">我的工作<i class="nl-badge">2</i></a>
+            </li>
+            <li>
+                <a href="order-statu.html">我的訂單<i class="nl-badge">1</i></a>
+            </li>
+            <li>
+                <a href="<?=URL::to('/')?>/web/h_set">服務設定</a>
+            </li>
+            <li class="submenu" id="subA">
+            <a href="javascript:void(0)">
+                我的帳務<i class="fa fa-angle-down" aria-hidden="true"></i>
+                </a>
+                <div class="sub-list" id="boxA">
+                <a class="sub-item" href="feedback.html">回饋金</a>
+                <a class="sub-item" href="income.html">服務收入</a>
+                <a class="sub-item" href="pay.html">支出</a>
+                </div>
+            </li>
+            <li class="submenu" id="subB">
+                <a href="javascript:void(0)">
+                推薦賺回饋<i class="fa fa-angle-down" aria-hidden="true"></i>
+                </a>
+                <div class="sub-list" id="boxB">
+                  <a class="sub-item" href="javascript:void(0)" @click="set_recommend">推薦連結</a>
+                  <a class="sub-item" href="promote.html">介紹幫棒給新夥伴</a>
+                  <a class="sub-item" href="{{URL::to('/web/partners')}}">我的夥伴團隊</a>
+                  <a class="sub-item" href="total.html">我的夥伴團隊總營收</a>>
+                </div>
+            </li>
+            <li>
                     <a href="/web/calendar">我的行事曆</a>
-                </li>
-            </ul>
-        </div>
+            </li>
+        </ul>
         @endif
+    </div>
     <div class="user">
         <a href="javascript:void(0)" class="user-icon">
-            <img src="/images/user.png">
+            <img src="<?=URL::to('/')?>/images/user.png">
         </a>
         <div class="user-box">
             <div class="user-info">
+<!--
+                <div class="user-face"><img src="{{URL::to('/') . '/avatar/small/' . session()->get('usrPhoto')}}"></div>
+                <div class="user-score"><span class="user-name">{{session()->get('usrName')['last'] . session()->get('usrName')['first']}}</span>
+-->
                 <div class="user-face"><img :src="((pro && pro.photo)?'{{asset('/avatar/big')}}/'+pro.photo:'{{asset('/images/person-icon.jpg')}}')"></div>
                 <div class="user-score"><span class="user-name" v-text="((pro)?((pro.nick)?pro.nick:pro.last+pro.first):'')"></span>
-                    <span class="start">
-                    	<i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i> 
-                     </span>
-                     <span class="avg">4.9</span>
+                <span class="start"><i class="fa fa-star"
+                            aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"
+                            aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star"
+                            aria-hidden="true"></i> </span><span class="avg">4.9</span>
                     <!--<div class="income">年度收入 <span class="text-danger">$50,100</span></div> -->
                     <div class="feedback">年度回饋金 <span class="text-danger">$2,300</span></div>
                 </div>
@@ -134,7 +137,11 @@
                 </div>
             </div>
             <a class="user-item" href="coupon.html">我的優惠劵</a>
+            @if(session()->get('usr_type') == 0)
             <a class="user-item" href="javascript:void(0)" id="head-change">切換成幫手</a>
+            @else
+            <a class="user-item" href="javascript:void(0)" id="head-change">切換成會員</a>
+            @endif
             <a class="user-item" href="contact.html">聯絡幫棒</a>
             <a class="user-item" href="{{url('/web/logout')}}">登出</a>
         </div>
